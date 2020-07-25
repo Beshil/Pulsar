1bfb6f90bc122a391636601ff69a3a98d0fc6599
beshil <ersanbievbesh@gmail.com>
2020-07-25 17:23:36 +0300

    work with slider

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+    autoplay: 1500,
+    prevArrow:'<button type="button" class="slick-prev"><img src="../icons/prev.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../icons/next.svg"></button>'
+  });
+});
